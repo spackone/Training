9b0202840c7810b83a992d55be0fab5aeb3f7a05
C <cnspackman@gmail.com>
2023-03-15 11:36:35 -0600

    Update

--- Day 15 Coffee Machine.py
+++ Day 15 Coffee Machine.py
@@ -3,6 +3,7 @@ MENU = {
         "ingredients": {
             "water": 50,
             "coffee": 18,
+            "milk": 0,
         },
         "cost": 1.5,
     },
@@ -33,19 +34,19 @@ profit = 0
 xxx = True
 while xxx == True:
     drink_type = input('What would you like? (espresso/latte/cappuccino): ',)
-
+    
     def resource_check(res = drink_type):
         enough = bool
-        water_needed = MENU[res["ingredients"["water"]]]
+        water_needed = MENU[res]["ingredients"]["water"]
         water_availible = resources["water"]
-        coffee_needed = MENU[res["ingredients"["coffee"]]]
+        coffee_needed = MENU[res]["ingredients"]["coffee"]
         coffee_availible = resources["coffee"]
-        milk_needed = MENU[res["ingredients"["milk"]]]
+        milk_needed = MENU[res]["ingredients"]["milk"]
         milk_availible = resources["milk"]
         if water_needed > water_availible:
             statement = 'Sorry there is not enough water'
             enough = False
-            return statement, enough
+            return enough, statement
         elif coffee_needed > coffee_availible:
             statement = 'Sorry there is not enough coffee'
             enough = False
@@ -56,30 +57,33 @@ while xxx == True:
             return enough, statement
         else: 
             enough = True
+            statement = ''
             return enough, statement
 
     def resource_update(res = drink_type):
         stop = True
+        total = 0
         while stop:
             quarters = int(input('How many quarters? '))
             dimes = int(input('How many dimes? '))
             nickels = int(input('How many nickels? '))
             pennies = int(input('How many pennies? '))
-            money_due = MENU[res["cost"]]
-            total = .25*quarters + .1*dimes + .05*nickels + .01*pennies
+            money_due = MENU[res]["cost"]
+            total += .25*quarters + .1*dimes + .05*nickels + .01*pennies
             if total < money_due:
                 short = total - money_due
-                proceed = input(f'You are short ${short}. \nWould you like to add more money? (y/n)')
+                proceed = input(f'You are short ${round(short, 2)}. \nWould you like to add more money? (y/n)')
                 if proceed != 'y':
-                    return print(f'Here is your ${total} back. ')
+                    money_due = 0
+                    return print(f'Here is your ${round(total, 2)} back. ')
                 else:
                     continue
             else:
-                resources["water"] -= MENU[res["ingredients"["water"]]]
-                resources["coffee"] -= MENU[res["ingredients"["coffee"]]]
-                resources["milk"] -= MENU[res["ingredients"["milk"]]]
+                resources["water"] -= MENU[res]["ingredients"]["water"]
+                resources["coffee"] -= MENU[res]["ingredients"]["coffee"]
+                resources["milk"] -= MENU[res]["ingredients"]["milk"]
                 change = money_due - total
-                print(f'${change} is your change. ')
+                print(f'${round(change, 2)} is your change. ')
                 stop = False
         
         return money_due
@@ -101,8 +105,11 @@ while xxx == True:
     else:
         enough, statement = resource_check(drink_type)
         if enough == True:
-            profit += resource_update(drink_type)
+            money_made = resource_update(drink_type)
+            profit += money_made
             print(f'Here is your {drink_type}. Enjoy!')
+        else:
+            print(statement)
             
         
 
